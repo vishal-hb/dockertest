1f315476833cb94d24ff80fc71f50a2c33c6bb18
root <root@ip-172-31-3-245.ap-south-1.compute.internal>
2022-12-29 07:07:19 +0000

    Add existing project files to Git

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title> Hello World!</title>
+		<meta name="refrence" content="https://www.atlantic.net/vps-hosting/how-to-deploy-a-php-application-with-nginx-and-mysql-using-docker-and-docker-compose"/>
+	</head>
+	
+	<body>
+		<h1>Hello World! This is updated Files form host</h1>
+		<p><? echo 'We are running PHP, versio: ' . phpversion();  ?></p>
+	</body>
+</html>
---
+++ src/index2.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title> Hello World!</title>
+	</head>
+	
+	<body>
+		<h1>Hello World!</h1>
+		<p><? echo 'We are running PHP, versio: ' . phpversion();  ?></p>
+		<?
+			$database = "mydb";
+			#$user = "myuser";
+			#$password = "password";
+			$user = "root";
+			$password = "secret";
+			$host = "mysql";
+			
+			$connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);  
+      			$query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");  
+       			$tables = $query->fetchAll(PDO::FETCH_COLUMN);  
+
+       			if (empty($tables)) {
+          			echo "<p>There are no tables in database \"{$database}\".</p>";
+        		} else {
+          			echo "<p>Database \"{$database}\" has the following tables:</p>";
+          			echo "<ul>";
+            				foreach ($tables as $table) {
+              					echo "<li>{$table}</li>";
+            				}
+          			echo "</ul>";
+        		}
+        	?>
+	</body>
+</html>
